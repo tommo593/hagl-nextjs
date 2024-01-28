26031f804bc235604323e13096bd07589ae11173
tommo593 <tomdevwork@gmail.com>
2024-01-28 15:31:56 +0000

    meh

---
+++ src/app/contact.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Phone: <a href="tel:+4203333111">+420 3333 111</a>
+      </p>
+      <p>
+        Email: <a href="mailto:contact@hagl.com">contact@hagl.com</a>
+      </p>
+    </div>
+  );
+}
